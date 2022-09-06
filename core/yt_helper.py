ee5568e27cd69cbdbfdc5ea0036878b5b9394b33
Johannes <johpro.knittel@gmail.com>
2022-09-06 19:01:09 +0200

    youtube api updates

--- core/yt_helper.py
+++ core/yt_helper.py
@@ -1,4 +1,6 @@
+from pydoc import describe
 import sys
+import io
 import os
 import http.client
 import httplib2
@@ -7,6 +9,7 @@ from apiclient.http import MediaFileUpload
 from apiclient.errors import HttpError
 import argparse
 from typing import List
+from datetime import timezone, datetime, timedelta
 
 import core.auth as conf_auth
 
@@ -23,7 +26,99 @@ class YouTubeHelper:
         """
         self.auth = conf_auth.Authentication(youtube=True, use_pickled_credentials=True)
 
+    def make_youtube_title(self, title : str) -> str:
+        """Make sure title is valid for Youtube: <= 100 characters and no '<' or '>' symbols
+        """
+        if title is None:
+            return None
+            
+        title = title.replace("<", " ").replace(">", " ")
+        if len(title) > 100:
+            title = title[0:99]
+        return title
+
+    def make_youtube_description(self, description : str) -> str:
+        """Similar rules for the description as the title, but max length of 5000 characters
+        """
+        if description is None:
+            return None
+        description = description.replace("<", " ").replace(">", " ")
+        if len(description) > 5000:
+            description = description[0:4999]
+        return description
+
+    def create_playlist(self, title : str):
+        """Create playlist.
+
+        Returns something like {
+                  "kind": "youtube#playlist",
+                  "etag": "7s-K....",
+                  "id": "PL...",
+                  "snippet": {
+                    "publishedAt": "2022-09-06T13:48:25Z",
+                    "channelId": "UCBJ....",
+                    "title": "This is a test playlist automatically generated",
+                    "description": "",
+                    "thumbnails": {
+                      "default": {
+                        "url": "https://i.ytimg.com/img/no_thumbnail.jpg",
+                        "width": 120,
+                        "height": 90
+                      },
+                      "medium": {
+                        "url": "https://i.ytimg.com/img/no_thumbnail.jpg",
+                        "width": 320,
+                        "height": 180
+                      },
+                      "high": {
+                        "url": "https://i.ytimg.com/img/no_thumbnail.jpg",
+                        "width": 480,
+                        "height": 360
+                      }
+                    },
+                    "channelTitle": "IEEE Visualization Conference",
+                    "localized": {
+                      "title": "This is a test playlist automatically generated",
+                      "description": ""
+                    }
+                  },
+                  "status": {
+                    "privacyStatus": "unlisted"
+                  }
+                }
+        """
+        title = self.make_youtube_title(title)
+        resp = self.auth.youtube.playlists().insert(
+            part="id,status,snippet",
+            body={
+                "snippet": {
+                    "title": title
+                },
+                "status": {
+                    "privacyStatus": "unlisted"
+                }
+            }).execute()
+        return resp
+
+    def add_video_to_playlist(self, playlist_id : str, video_id : str):
+        """Add existing video to existing playlist
+        """
+        resp = self.auth.youtube.playlistItems().insert(
+                part="id,status,snippet",
+                body={
+                    "snippet": {
+                        "playlistId": playlist_id,
+                        "resourceId": {
+                            "kind": "youtube#video",
+                            "videoId": video_id
+                        }
+                    }
+                }).execute()
+        return resp
+        
     def upload_video(self, path : str, title : str, description : str):
+        title = self.make_youtube_title(title)
+        description = self.make_youtube_description(description)
         upload_request = self.auth.youtube.videos().insert(
             part="id,status,snippet",
             body = {
@@ -79,7 +174,9 @@ class YouTubeHelper:
         return None
 
     def update_video(self, video_id : str, title : str, description : str):
-        print("Updating\ntitle = {}\nvideo = {}".format(title, video_id))
+        #print("Updating\ntitle = {}\nvideo = {}".format(title, video_id))
+        title = self.make_youtube_title(title)
+        description = self.make_youtube_description(description)
         upload_response = self.auth.youtube.videos().update(
             part="id,snippet,status",
             body = {
@@ -92,6 +189,127 @@ class YouTubeHelper:
             }
         ).execute()
         return upload_response
+
+    def set_video_embeddable(self, video_id : str):
+        """Make sure that 'embeddable' is set to True of specified video
+        (e.g., after live broadcast has stoppped)
+        """
+        resp = self.auth.youtube.videos().update(
+            part="id,contentDetails,status",
+            body={
+                "id": video_id,
+                "status": {
+                    "embeddable": True,
+                }
+            }
+        ).execute()
+        return resp
+
+    def upload_subtitles(self, video_id : str, subtitles_path : str, name : str = "English Subtitles",
+                        language : str = "en-us"):
+        """Upload subtitles file to specified video
+        """
+        resp = self.auth.youtube.captions().insert(
+                    part="id,snippet",
+                    body={
+                        "snippet": {
+                            "videoId": video_id,
+                            "language": language,
+                            "name": name
+                        }
+                    },
+                    media_body=MediaFileUpload(subtitles)
+                ).execute()
+        return resp
+
+    def schedule_broadcast(self, title : str, description : str, start_time : datetime, enable_captions : bool = False,
+                           thumbnail_png_bytes : io.BytesIO = None, thumbnail_path : str = None):
+        """Schedule a broadcast
+
+        enable_captions: if True, "closedCaptionsHttpPost" will be used and latencyPreference will be set to "low" instead of "ultraLow"
+        thumbnail_png_bytes: optional thumbnail image provided as rendered image bytes
+        thumbnail_path: optional thumbnail image, path to file
+        """
+        title = self.make_youtube_title(title)
+        description = self.make_youtube_description(description)
+        broadcast_info = self.auth.youtube.liveBroadcasts().insert(
+            part="id,snippet,contentDetails,status",
+            body={
+                "contentDetails": {
+                    "closedCaptionsType": "closedCaptionsHttpPost" if enable_captions else "closedCaptionsDisabled",
+                    "enableContentEncryption": False,
+                    "enableDvr": True,
+                    # Note: YouTube requires you to have 1k subscribers and 4k public watch hours
+                    # to enable embedding live streams. You can set this to true if your account
+                    # meets this requirement and you've enabled embedding live streams
+                    "enableEmbed": True,
+                    "enableAutoStart": False,
+                    "enableAutoEnd": False,
+                    "recordFromStart": True,
+                    "startWithSlate": False,
+                    # We must use a low latency only stream if using live captions
+                    "latencyPreference": "low" if enable_captions else "ultraLow",
+                    "monitorStream": {
+                        "enableMonitorStream": False,
+                        "broadcastStreamDelayMs": 0
+                    }
+                },
+                "snippet": {
+                    "title": title,
+                    "scheduledStartTime": start_time.astimezone(tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.0Z"),
+                    "description": description,
+                },
+                "status": {
+                    "privacyStatus": "unlisted"
+                }
+            }
+        ).execute()
+
+        # Due to a bug in the Youtube Broadcast API we have to set the made for
+        # kids and embeddable flags through the videos API separately
+        update_resp = self.auth.youtube.videos().update(
+            part="id,contentDetails,status",
+            body={
+                "id": broadcast_info["id"],
+                "status": {
+                    "selfDeclaredMadeForKids": False,
+                    "embeddable": True
+                }
+            }
+        ).execute()
+
+        # Render the thumbnail for the session and upload it
+        if thumbnail_png_bytes:
+            self.auth.youtube.thumbnails().set(
+                videoId=broadcast_info["id"],
+                media_body=MediaIoBaseUpload(thumbnail_png_bytes, mimetype="image/png")
+            ).execute()
+        elif thumbnail_path:
+            self.auth.youtube.thumbnails().set(
+                videoId=broadcast_info["id"],
+                media_body=MediaFileUpload(thumbnail_path)
+            ).execute()
+
+
+        return broadcast_info
+
+    def get_broadcasts(self) -> List:
+        """get all broadcasts of associated channel (mine)
+        """
+        all_items = []
+        page_token = None
+        while True:
+            items = self.auth.youtube.liveBroadcasts().list(
+                part="id,snippet,contentDetails,status",
+                maxResults=50,
+                mine=True,
+                pageToken=page_token
+            ).execute()
+            all_items += items["items"]
+            if "nextPageToken" not in items:
+                break
+            page_token = items["nextPageToken"]
+        return all_items
 
     def get_all_playlists(self) -> List:
         """get all playlists of current user
@@ -255,3 +473,49 @@ class YouTubeHelper:
             res.append(res_item)
         return res
 
+    def get_stream_status(self, stream_key_id : str):
+        """get streamStatus ("active", "created", "error", "inactive", "ready"), healthStatus ("good", "ok", "bad", "noData") of specified stream
+        """
+        response = self.auth.youtube.liveStreams().list(
+            id=stream_key_id,
+            part="status"
+        ).execute()
+        return response["items"][0]["status"]["streamStatus"], response["items"][0]["status"]["healthStatus"]["status"]
+
+    def get_broadcast_status(self, broadcast_id : str):
+        """return broadcast lifeCycleStatus of specified broadcast, e.g. "ready", "live" or "complete" or "testing"
+        """
+        response = self.auth.youtube.liveBroadcasts().list(
+            id=broadcast_id,
+            part="status"
+        ).execute()
+        return response["items"][0]["status"]["lifeCycleStatus"]
+        
+    def set_broadcast_status(self, broadcast_id : str, status : str):
+        """set status of specified broadcast, e.g. to "live" or "complete" or "testing"
+        """
+        resp = self.auth.youtube.liveBroadcasts().transition(
+            broadcastStatus=status,
+            id=broadcast_id,
+            part="status"
+        ).execute()
+        return resp
+
+    def get_broadcast_statistics(self, broadcast_id : str):
+        """return liveStreamingDetails of specified broadcast
+        """
+        response = self.auth.youtube.videos().list(
+            id=broadcast_id,
+            part="liveStreamingDetails"
+        ).execute()
+        return response["items"][0]["liveStreamingDetails"]
+
+    def bind_stream_to_broadcast(self, stream_key_id : str, broadcast_id : str):
+        """attach specified stream to specified broadcast or remove it by setting stream_key_id to None
+        """
+        resp = self.auth.youtube.liveBroadcasts().bind(
+            id=broadcast_id,
+            part="status",
+            streamId=stream_key_id,
+        ).execute()
+        return resp
--- yt_api.py
+++ yt_api.py
@@ -2,6 +2,8 @@ import sys
 import os
 import time
 import argparse
+import json
+from datetime import datetime
 
 from core.yt_helper import YouTubeHelper
 
@@ -10,16 +12,47 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Script to perform various API actions for YouTube.')
 
+    
     parser.add_argument('--playlists', help='retrieve playlists',
                         action='store_true', default=False)
     parser.add_argument('--playlists_items', help='retrieve playlists and all items',
                         action='store_true', default=False)
+    parser.add_argument('--create_playlist', help='create playlist',
+                        action='store_true', default=False)
+    
+    parser.add_argument('--broadcasts', help='retrieve broadcasts',
+                        action='store_true', default=False)
+    parser.add_argument('--schedule_broadcast', help='schedule broadcast',
+                        action='store_true', default=False)
+    parser.add_argument('--upload_video', help='upload video',
+                        action='store_true', default=False)
+    
+    parser.add_argument('--title', help='title of item (e.g., video)', default=None)
+    parser.add_argument('--description', help='description of item (e.g., video)', default=None)
+    parser.add_argument('--start_time', help='start time of scheduled broadcast in the "%Y-%m-%d %H:%M" format in your local time zone', default=None)
+    parser.add_argument('--path', help='path to file that should be uploaded, e.g.', default=None)
+
+    
+    args = parser.parse_args()
 
     yt = YouTubeHelper()
 
     if args.playlists:
         playlists = yt.get_all_playlists()
-        print(playlists)
+        print(json.dumps(playlists))
     elif args.playlists_items:
         res = yt.get_playlists_and_videos()
-        print(res)
+        print(json.dumps(res))
+    elif args.create_playlist:
+        pl = yt.create_playlist(args.title)
+        print(json.dumps(pl))
+    elif args.broadcasts:
+        res = yt.get_broadcasts()
+        print(json.dumps(res))
+    elif args.schedule_broadcast:
+        dt = datetime.strptime(args.start_time, "%Y-%m-%d %H:%M")
+        res = yt.schedule_broadcast(args.title, args.description, dt)
+        print(json.dumps(res))
+    elif args.upload_video:
+        res = yt.upload_video(args.path, args.title, args.description)
+        print(json.dumps(res))
