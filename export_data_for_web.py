6cb3da79ce7c425972099503779fe5af66ffcc09
John Thompson <johnthompson@microsoft.com>
2022-10-07 06:42:03 -0400

    Coordinate with broadcasts and tracks tables. Add strip for list fields.

--- export_data_for_web.py
+++ export_data_for_web.py
@@ -10,6 +10,7 @@ import argparse
 from core.auth import Authentication
 from core.google_sheets import GoogleSheets
 
+
 def parse_time(t: str):
     return datetime.fromisoformat(t.replace("Z", "+00:00"))
 
@@ -76,7 +77,7 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
     sheet_broadcasts.load_sheet("Broadcasts")
     broadcasts_dict = dict()
     for b in sheet_broadcasts.data:
-        broadcasts_dict[b["Livestream ID"]]
+        broadcasts_dict[b["Livestream ID"]] = b
 
     # ['Event', 'Event Type', 'Event Prefix', 'Event Description',
     #  'Event URL', 'Organizers', 'Organizer Emails']
@@ -89,7 +90,7 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
             "event_prefix": e["Event Prefix"],
             "event_description": e["Event Description"],
             "event_url": e["Event URL"],
-            "organizers": e["Organizers"].split("|") if e["Organizers"] else [],
+            "organizers": [o.strip() for o in e["Organizers"].split("|")] if e["Organizers"] else [],
             "sessions": []
         }
 
@@ -97,8 +98,6 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
             all_events[e_data["event_prefix"]] = e_data
         else:
             all_events[e_data["event_prefix"]].append(e_data)
-
-    print(all_events)
 
     # ['Session ID', 'Event Prefix', 'Session Title', 'DateTime Start', 'DateTime End',
     # 'Track', 'Livestream ID', 'Session Image', 'Session Chairs', 'Session Chairs EMails',
@@ -109,8 +108,9 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
 
     # Create session data
     for s in sheet_sessions.data:
-        t = tracks_dict[s["Track"]]
-        b = broadcasts_dict[s["LivestreamID"]]
+        t = tracks_dict[s["Track"]] if s["Track"] in tracks_dict else None
+        b = broadcasts_dict[s["Livestream ID"]
+                            ] if s["Livestream ID"] in broadcasts_dict else None
 
         s_data = {
             "title": s["Session Title"],
@@ -119,7 +119,7 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
             "track": s["Track"],
             "livestream_id": s["Livestream ID"],
             "session_image": f'{s["Session ID"]}.png',
-            "chair": s["Session Chairs"].split("|") if s["Session Chairs"] else [],
+            "chair": [c.strip() for c in s["Session Chairs"].split("|")] if s["Session Chairs"] else [],
             "organizers": [],
             "time_start": format_time_iso8601_utc(parse_time(s["DateTime Start"])),
             "time_end": format_time_iso8601_utc(parse_time(s["DateTime End"])),
@@ -128,7 +128,7 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
             "discord_channel_id": t["Discord Channel ID"] if t else "",
             "discord_link": t["Discord URL"] if t else "",
             "slido_link": t["Slido URL"] if t else "",
-            "youtube_url": b["YouTube URL"] if b else "",
+            "youtube_url": b["YouTube URL"] if b else "https://youtu.be/_evorVC17Yg",
             "zoom_meeting": s["Zoom Meeting ID"],
             "zoom_password": s["Zoom Password"],
             "zoom_link": s["Zoom URL"],
@@ -144,7 +144,8 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
         for p in filtered_papers:
             # Find the corresponding entry by Paper UID in PapersDB
 
-            p_db = db_papers_dict[p["Paper UID"]]
+            p_db = db_papers_dict[p["Paper UID"]
+                                  ] if p["Paper UID"] in db_papers_dict else None
 
             p_event_prefix = p_db["Event Prefix"] if p_db else ""
 
@@ -167,8 +168,8 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
                 "session_id": p["Session ID"],
                 "type": p["Slot Type"],
                 "title": p["Slot Title"],
-                "contributors": p["Slot Contributors"].split("|") if p["Slot Contributors"] else [],
-                "authors": p["Authors"].split("|") if p["Authors"] else [],
+                "contributors": [c.strip() for c in p["Slot Contributors"].split("|")] if p["Slot Contributors"] else [],
+                "authors": [a.strip() for a in p["Authors"].split("|")] if p["Authors"] else [],
                 "abstract": p_db["Abstract"] if p_db else "",
                 "uid": p["Paper UID"],
                 "file_name": p["File Name"],
@@ -177,11 +178,11 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
                 "time_end": format_time_iso8601_utc(parse_time(p["Slot DateTime End"])),
                 # "youtube_video_id": p["YouTube Video"],
                 "paper_type": paper_type,
-                "keywords": p_db["Keywords"].split("|") if p_db and p_db["Keywrods"] else [],
+                "keywords": [k.strip() for k in p_db["Keywords"].split("|")] if p_db and p_db["Keywords"] else [],
                 "has_image": p_db["Has Image"] if p_db else False,
                 "has_video": p_db["Has Video"] if p_db else False,
                 "has_video": p_db["Video Duration"] if p_db else "",
-                "paper_award": p_db["Paper Award"] if p_db else "",
+                "paper_award": p_db["Award"] if p_db else "",
                 "image_caption": p_db["Image Caption"] if p_db else "",
                 "external_paper_link": "",
                 "has_pdf": False,
@@ -213,8 +214,8 @@ def create_data_for_web(auth: Authentication, output_dir: str, export_ics: bool,
             "uid": p["UID"],
             "discord_channel": "",
             "has_image": False,
-            "authors": p["Authors"].split("|") if p["Authors"] else [],
-            "author_affiliations": p["ACM Author Affiliations"].split(";") if p["ACM Author Affiliations"] else [],
+            "authors": [a.strip() for a in p["Authors"].split("|")] if p["Authors"] else [],
+            "author_affiliations": [a.strip() for a in p["ACM Author Affiliations"].split(";")] if p["ACM Author Affiliations"] else [],
             "presenting_author": p["Presenting Author (name)"],
             "abstract": p["Abstract"],
             "has_summary_pdf": p["PDF Link"] != "",
