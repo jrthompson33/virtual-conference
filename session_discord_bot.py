6333b539ac00bc5a5b21b4f7354226e5f290ed07
Johannes Knittel <johpro.knittel@gmail.com>
2023-10-22 08:30:47 +1100

    update discord bot for 2023

--- session_discord_bot.py
+++ session_discord_bot.py
@@ -16,9 +16,17 @@ general_channel = None
 sessions_sheet = GoogleSheets()
 sessions_sheet.load_sheet("Sessions")
 
+
 sessions = [(datetime.fromisoformat(s["DateTime Start"].replace('Z', '+00:00')) - timedelta(minutes=5), datetime.fromisoformat(s["DateTime End"].replace('Z', '+00:00')), s) for s in sessions_sheet.data]
 session_id_to_channel = {}
 
+sheet_tracks = GoogleSheets()
+sheet_tracks.load_sheet("Tracks")
+tracks_dict = dict()
+for t in sheet_tracks.data:
+    tracks_dict[t["Track"]] = t
+
+    
 last_tick = datetime.now(timezone.utc) - timedelta(minutes=1.0)
 
 @client.event
@@ -40,7 +48,11 @@ async def on_ready():
         sid = s["Session ID"]
         if(len(sid.strip()) == 0):
             continue
-        ch_id_s = s["Discord Channel ID"].strip()
+        tr = s["Track"]
+        if tr is None or tr not in tracks_dict:
+            continue
+        track = tracks_dict[tr]
+        ch_id_s = track["Discord Channel ID"].strip()
         if(len(ch_id_s) == 0):
             continue
         ch_id = int(ch_id_s)
@@ -79,12 +91,12 @@ async def post_session_info():
             ch = session_id_to_channel[sid]
             title = session["Session Title"]
             track = session["Track"]
-            if len(track) == 0 or track == "various":
+            if len(track) == 0 or track == "various" or track == "none":
                 continue
             print(f"{dt} sending message")
             await ch.send(content=f"The session **{title}** in this track is going to start in just a few minutes.\r\nCheck the session overview on our virtual website: https://virtual.ieeevis.org/year/2022/session_{sid}.html\r\nor watch the livestream at https://virtual.ieeevis.org/year/2022/room_{track}.html\r\nPlease use this channel for fruitful exchanges, but note that session chairs will not pick up questions on Discord for Q+A.")
     last_tick = dt
 
-
-post_session_info.start()
+asyncio.run(post_session_info())
+#post_session_info.start()
 client.run(auth.discord["bot_token"])
